7645b6eaeb180a9623c422c16e474496024344a4
tamiresslelis <tamiresslelis@gmail.com>
2019-09-11 22:58:37 -0300

    adicionando conteudo

--- js/app.js
+++ js/app.js
@@ -1,28 +1,28 @@
 // Função para o header ter uma cor diferente quando scrolla 
-var nav = document.querySelector('nav'); // Identify target
+// var nav = document.querySelector('nav'); // Identify target
 
-window.addEventListener('scroll', function (event) { // To listen for event
-    event.preventDefault();
+// window.addEventListener('scroll', function (event) { // To listen for event
+//     event.preventDefault();
 
-    if (window.scrollY <= 150) { // Just an example
-        nav.style.backgroundColor = 'white'; // or default color
-        nav.style.borderBottom = "2px solid white";
-    } else {
-        this.console.log(nav.style);
-        nav.style.borderBottom = "2px solid black";
+//     if (window.scrollY <= 150) { // Just an example
+//         nav.style.backgroundColor = 'white'; // or default color
+//         nav.style.borderBottom = "2px solid white";
+//     } else {
+//         this.console.log(nav.style);
+//         nav.style.borderBottom = "2px solid black";
 
-    }
-});
+//     }
+// });
 
 // Função para abrir o menu device mobile
 document.getElementById('menu-btn').addEventListener('click', function (e) {
-    
+
     // variable for affected menu
     let myMenu = document.getElementById('menu-mobile');
 
     if (myMenu.style.display === 'block') {
         myMenu.style.display = 'none';
     } else {
-       myMenu.style.display = 'block';
+        myMenu.style.display = 'block';
     }
 });
